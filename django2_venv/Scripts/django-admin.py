ede121a57861f6dee21eca858fb20af24f4dc565
alves-Moises <moisesinho_ra@hotmail.com>
2022-09-11 14:47:06 -0300

    first commit

---
+++ django2_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\coding\Python\Estudos\django_geek_university\sessao_04\django2\django2_venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
