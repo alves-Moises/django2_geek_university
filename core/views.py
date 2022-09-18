43a00020457278ae6054596ab7bf71395585b00e
alves-Moises <moisesinho_ra@hotmail.com>
2022-09-17 21:02:26 -0300

    new: form

--- core/views.py
+++ core/views.py
@@ -1,10 +1,38 @@
+from email import message
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContatoForm
+
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: {nome}')
+            print(f'Email: {email}')
+            print(f'Nome: {assunto}')
+            print(f'Nome: {mensagem}')
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        
+        else:
+            messages.error(request, 'erro ao enviar email')
+    context = {
+        'form': form
+    }
+
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')
