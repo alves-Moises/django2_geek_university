60d95d495eefd80cffd882e65f12c02edcb8a58f
alves-Moises <moisesinho_ra@hotmail.com>
2022-09-28 16:13:02 -0300

    forms.py: send_mail() function

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,24 @@
-from sys import maxsize
 from django import forms
+from django.core.mail.message import EmailMessage
 
 class ContatoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
     email = forms.EmailField(label="E-mail", max_length=100)
     assunto = forms.CharField(label="Assunto", max_length=120)
     mensagem = forms.CharField(label="Mensagem", widget=forms.Textarea())
+
+    def send_mail(self):
+        nome = self.cleaned_data['nome']
+        email = self.cleaned_data['email']
+        assunto = self.cleaned_data['assunto']
+        mensagem = self.cleaned_data['mensagem']
+
+        conteudo = f'nome: {nome}\n Email: {email}\n assunto: {assunto}\n Mensgem: {mensagem}'
+        mail = EmailMessage(
+            subject='Email enviado pelo sistema DJango2',
+            body=conteudo,
+            from_email='contato@dominio.com.br',
+            to=['contato@seudominio.com.br'],
+            headers={'Reply-To': email}
+        )
+        mail.send()
