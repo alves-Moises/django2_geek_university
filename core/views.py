ef20c3ad934aae6a7eb13a58fc606b5cb546efee
alves-Moises <moisesinho_ra@hotmail.com>
2022-09-28 16:13:34 -0300

    update views

--- core/views.py
+++ core/views.py
@@ -12,16 +12,7 @@ def contato(request):
 
     if str(request.method) == 'POST':
         if form.is_valid():
-            nome = form.cleaned_data['nome']
-            email = form.cleaned_data['email']
-            assunto = form.cleaned_data['assunto']
-            mensagem = form.cleaned_data['mensagem']
-
-            print('Mensagem enviada')
-            print(f'Nome: {nome}')
-            print(f'Email: {email}')
-            print(f'Nome: {assunto}')
-            print(f'Nome: {mensagem}')
+            form.send_mail()
 
             messages.success(request, 'E-mail enviado com sucesso!')
             form = ContatoForm()
